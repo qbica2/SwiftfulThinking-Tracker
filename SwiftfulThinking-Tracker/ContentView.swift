4ecf2070935dbbab1a83ff447bc17898ffb5ce48
Kubilay Akdemir <kubidik27@gmail.com>
2023-06-04 13:28:25 +0300

    Initial Commit

---
+++ SwiftfulThinking-Tracker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftfulThinking-Tracker
+//
+//  Created by Mehmet Kubilay Akdemir on 4.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftfulThinking-Tracker/SwiftfulThinking_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinking_TrackerApp.swift
+//  SwiftfulThinking-Tracker
+//
+//  Created by Mehmet Kubilay Akdemir on 4.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinking_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
