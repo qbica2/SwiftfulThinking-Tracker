714dea2f8076acbe1d1fdd53a94c468f85bdd34f
Kubilay Akdemir <kubidik27@gmail.com>
2023-06-08 00:11:28 +0300

    learn basic Animations

---
+++ SwiftfulThinking-Tracker/AnimationBootcamp.swift
@@ -0,0 +1,39 @@
+//
+//  AnimationBootcamp.swift
+//  SwiftfulThinking-Tracker
+//
+//  Created by Mehmet Kubilay Akdemir on 7.06.2023.
+//
+
+import SwiftUI
+
+struct AnimationBootcamp: View {
+    
+    @State var isAnimated: Bool = false
+    
+    var body: some View {
+        VStack {
+            Button("Button") {
+//                withAnimation (Animation.default) {
+                    isAnimated.toggle()
+//                }
+            }
+            Spacer()
+            RoundedRectangle(cornerRadius: isAnimated ? 50: 25)
+                .fill(isAnimated ? Color.red : Color.green)
+                .frame(
+                    width: isAnimated ? 100 : 300,
+                    height: isAnimated ? 100 : 300)
+                .rotationEffect(Angle(degrees: isAnimated ? 360 : 0))
+                .offset(y: isAnimated ? 300: 0)
+                .animation(.default.repeatCount(2), value: isAnimated)
+            Spacer()
+        }
+    }
+}
+
+struct AnimationBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationBootcamp()
+    }
+}
