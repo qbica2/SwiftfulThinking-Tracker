78b6687e2341660888bd8ae9f8aa900094f3083c
Kubilay Akdemir <kubidik27@gmail.com>
2023-06-08 13:42:33 +0300

    learn transition

---
+++ SwiftfulThinking-Tracker/TransitionBootcamp.swift
@@ -0,0 +1,46 @@
+//
+//  TransitionBootcamp.swift
+//  SwiftfulThinking-Tracker
+//
+//  Created by Mehmet Kubilay Akdemir on 8.06.2023.
+//
+
+import SwiftUI
+
+struct TransitionBootcamp: View {
+    
+    @State var showView: Bool = false
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            VStack {
+                Button("Button") {
+                    showView.toggle()
+                }
+                Spacer()
+            }
+            
+            if showView {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(height: UIScreen.main.bounds.height * 0.5)
+//                    .transition(.move(edge: .bottom))
+//                    .animation(.spring())
+//                    .animation(.easeInOut)
+//                    .transition(AnyTransition.opacity.animation(.easeInOut))
+//                    .transition(AnyTransition.scale.animation(.easeInOut))
+                    .transition(.asymmetric(
+                        insertion: .move(edge: .bottom),
+                        removal: .move(edge: .trailing)
+                    ))
+                    .animation(.easeInOut)
+            }
+        }
+        .ignoresSafeArea(edges: .bottom)
+    }
+}
+
+struct TransitionBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TransitionBootcamp()
+    }
+}
