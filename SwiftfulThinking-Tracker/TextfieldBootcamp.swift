dc81d9602f1aaf8b72ec8435b57f8a654168d6cc
Kubilay Akdemir <kubidik27@gmail.com>
2023-06-09 16:00:34 +0300

    learn TextField

---
+++ SwiftfulThinking-Tracker/TextfieldBootcamp.swift
@@ -0,0 +1,83 @@
+//
+//  TextfieldBootcamp.swift
+//  SwiftfulThinking-Tracker
+//
+//  Created by Mehmet Kubilay Akdemir on 9.06.2023.
+//
+
+import SwiftUI
+
+struct TextfieldBootcamp: View {
+    
+    @State var textFieldText: String = ""
+    @State var dataArray: [String] = []
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                TextField("Type something here ...", text: $textFieldText)
+                    .padding()
+                    .background(.gray.opacity(0.4))
+                    .cornerRadius(10)
+                    .font(.headline)
+                    .foregroundColor(.red)
+                
+                Button {
+                    if isTextFieldValid() {
+                        saveText()
+                    }
+                } label: {
+                    Text("Save".uppercased())
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(isTextFieldValid() ? .blue : .gray)
+                        .cornerRadius(10)
+                        .font(.headline)
+                        .foregroundColor(.red)
+                }
+                .disabled(!isTextFieldValid())
+                
+                List {
+                    Section {
+                        ListSection(items: $dataArray)
+                    } header: {
+                        if dataArray.count != 0 {
+                            ListHeader(title: "Todos", logo: Image(systemName: "list.bullet.clipboard"))
+                        }
+                       
+                    }
+                    
+                }
+                .listStyle(.inset)
+                
+                Spacer()
+            }
+            .padding()
+            
+            .navigationTitle("TextField Bootcamp!")
+        }
+        
+    }
+    
+    func isTextFieldValid() -> Bool {
+        
+        let trimmedText = textFieldText.trimmingCharacters(in: .whitespaces)
+
+        if trimmedText.count >= 3 {
+            return true
+        }
+        
+        return false
+    }
+    
+    func saveText(){
+        dataArray.append(textFieldText)
+        textFieldText = ""
+    }
+}
+
+struct TextfieldBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextfieldBootcamp()
+    }
+}
